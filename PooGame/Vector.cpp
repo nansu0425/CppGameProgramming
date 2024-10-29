41d3aa29c894c52c6b3547bafd380d83d30d26a4
nansu0425 <nansu0425@gmail.com>
2024-10-29 15:08:23 +0900

    Beginner C++ Game Programming DirectX [2D Vectors] Tutorial 19
    2차원 Vector 구현

---
+++ PooGame/Vector.cpp
@@ -0,0 +1,70 @@
+﻿#include "Vector.h"
+
+#include <cmath>
+
+Vector::Vector(float x, float y)
+	: x(x)
+	, y(y)
+{}
+
+float Vector::GetLength() const
+{
+	return std::sqrt(std::pow(x, 2.0f) + std::pow(y, 2.0f));
+}
+
+Vector Vector::GetNormalized() const
+{
+	return *this / GetLength();
+}
+
+Vector Vector::operator+(const Vector& rhs) const
+{
+	return Vector(x + rhs.x, y + rhs.y);
+}
+
+Vector& Vector::operator+=(const Vector& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+
+	return *this;
+}
+
+Vector Vector::operator-(const Vector& rhs) const
+{
+	return Vector(x - rhs.x, y - rhs.y);
+}
+
+Vector& Vector::operator-=(const Vector& rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+
+	return *this;
+}
+
+Vector Vector::operator*(float rhs) const
+{
+	return Vector(x * rhs, y * rhs);
+}
+
+Vector& Vector::operator*=(float rhs)
+{
+	x *= rhs;
+	y *= rhs;
+
+	return *this;
+}
+
+Vector Vector::operator/(float rhs) const
+{
+	return Vector(x / rhs, y / rhs);
+}
+
+Vector& Vector::operator/=(float rhs)
+{
+	x /= rhs;
+	y /= rhs;
+
+	return *this;
+}
---
+++ PooGame/Vector.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+struct Vector
+{
+	Vector() = default;
+	Vector(float x, float y);
+
+	float		GetLength() const;
+	Vector		GetNormalized() const;
+
+	Vector		operator+(const Vector& rhs) const;
+	Vector&		operator+=(const Vector& rhs);
+	Vector		operator-(const Vector& rhs) const;
+	Vector&		operator-=(const Vector& rhs);
+	Vector		operator*(float rhs) const;
+	Vector&		operator*=(float rhs);
+	Vector		operator/(float rhs) const;
+	Vector&		operator/=(float rhs);
+
+	float		x = 0.0f;
+	float		y = 0.0f;
+};
