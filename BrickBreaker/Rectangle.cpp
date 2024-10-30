225b7fb151edce56fe56d6639073b717dc4c49d9
nansu0425 <nansu0425@gmail.com>
2024-10-30 23:42:54 +0900

    Beginner C++ Game Programming DirectX [Fart-Annoyed] Tutorial 20 - Pt. 1
    Rectangle 클래스 추가

---
+++ BrickBreaker/Rectangle.cpp
@@ -0,0 +1,10 @@
+﻿#include "Rectangle.h"
+
+Rectangle::Rectangle(const Vector& position)
+	: m_position(position)
+{}
+
+MovingRectangle::MovingRectangle(const Vector& position, const Vector& direction)
+	: Rectangle(position)
+	, m_direction(direction)
+{}
---
+++ BrickBreaker/Rectangle.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "Vector.h"
+
+class Graphics;
+
+class Rectangle
+{
+public:
+	virtual void	Update(float deltaTime) abstract;
+	virtual void	Draw(const Graphics& graphics) const abstract;
+
+protected:
+	Rectangle(const Vector& position);
+
+protected:
+	Vector			m_position;
+};
+
+class MovingRectangle : public Rectangle
+{
+protected:
+	MovingRectangle(const Vector& position, const Vector& direction);
+
+	virtual void	Move(float deltaTime) abstract;
+
+protected:
+	Vector			m_direction;
+};
